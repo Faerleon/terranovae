5addb391ef1fc14d017fc8c3b2520751cb7d28e8
Faerleon <ictao-accounts@protonmail.com>
2025-02-16 10:50:53 +0000

    move data into own file in test

--- test/calendarius/calendarius.unit.test.ts
+++ test/calendarius/calendarius.unit.test.ts
@@ -1,5 +1,5 @@
-import { TOptionsBaseUnit, TOptionsDerivedUnit } from '../../src/calendarius';
 import { createUnitDefinition } from '../../src/calendarius/createUnitDefinition.function';
+import { expectedDefinition } from './_data/expectedDefinition';
 
 describe('Calendarius', () => {
 	const generatedDefinition = createUnitDefinition(({ define, base }) => {
@@ -49,71 +49,7 @@ describe('Calendarius', () => {
 
 	it('should create a model correctly', () => {
 		expect(generatedDefinition).toStrictEqual({
-			definedSystem: new Map<
-				string,
-				TOptionsBaseUnit | TOptionsDerivedUnit
-			>([
-				['millisecond', { base: true }],
-				['second', { inBase: 1000 }],
-				['minute', { inBase: 1000 * 60 }],
-				['hour', { inBase: 1000 * 60 * 60 }],
-				['day', { inBase: 1000 * 60 * 60 * 24 }],
-				['week', { inBase: 1000 * 60 * 60 * 24 * 7 }],
-				['month_feb', { inBase: 1000 * 60 * 60 * 24 * 28 }],
-				[
-					'month_feb_leap',
-					{
-						inBase: 1000 * 60 * 60 * 24 * 29,
-					},
-				],
-				['month_short', { inBase: 1000 * 60 * 60 * 24 * 30 }],
-				['month_long', { inBase: 1000 * 60 * 60 * 24 * 31 }],
-				[
-					'year',
-					{
-						inBase:
-							1000 * 60 * 60 * 24 * 31 + // JAN
-							1000 * 60 * 60 * 24 * 28 + // FEB
-							1000 * 60 * 60 * 24 * 31 + // MAR
-							1000 * 60 * 60 * 24 * 30 + // APR
-							1000 * 60 * 60 * 24 * 31 + // MAY
-							1000 * 60 * 60 * 24 * 30 + // JUN
-							1000 * 60 * 60 * 24 * 31 + // JUL
-							1000 * 60 * 60 * 24 * 31 + // AUG
-							1000 * 60 * 60 * 24 * 30 + // SEP
-							1000 * 60 * 60 * 24 * 31 + // OCT
-							1000 * 60 * 60 * 24 * 30 + // NOV
-							1000 * 60 * 60 * 24 * 31, // DEC
-					},
-				],
-				[
-					'year_leap',
-					{
-						inBase:
-							1000 * 60 * 60 * 24 * 31 + // JAN
-							1000 * 60 * 60 * 24 * 29 + // FEB (leap year)
-							1000 * 60 * 60 * 24 * 31 + // MAR
-							1000 * 60 * 60 * 24 * 30 + // APR
-							1000 * 60 * 60 * 24 * 31 + // MAY
-							1000 * 60 * 60 * 24 * 30 + // JUN
-							1000 * 60 * 60 * 24 * 31 + // JUL
-							1000 * 60 * 60 * 24 * 31 + // AUG
-							1000 * 60 * 60 * 24 * 30 + // SEP
-							1000 * 60 * 60 * 24 * 31 + // OCT
-							1000 * 60 * 60 * 24 * 30 + // NOV
-							1000 * 60 * 60 * 24 * 31, // DEC
-					},
-				],
-				[
-					'cycle',
-					{
-						// 1 leap year
-						inBase:
-							1000 * 60 * 60 * 24 * 365 * 3 + // 3 normal years
-							1000 * 60 * 60 * 24 * 366,
-					},
-				],
-			]),
+			definedSystem: expectedDefinition,
 		});
 	});
 });
